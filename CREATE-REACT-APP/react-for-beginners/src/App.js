bf081f7b0dbf9e49dbdad1b554cd9dc6e93fba42
Claire <codeblanx@gmail.com>
2022-09-10 21:20:30 +0900

    [2022-09-10] study (movie app)

--- CREATE-REACT-APP/react-for-beginners/src/App.js
+++ CREATE-REACT-APP/react-for-beginners/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async() => {
-    const response = await fetch("https://yts.mx/api/v2/list_movies.json?%20minimum_rating=8.5&sort_by=year")
+    const response = await fetch("https://yts.mx/api/v2/list_movies.json?%20minimum_rating=8.8&sort_by=year")
     const json = await response.json();
     setMovies(json.data.movies);
     setLoading(false);
@@ -19,7 +19,14 @@ function App() {
       {loading ? <h1>loading...</h1> : (
         <div>
           {movies.map((movie) => 
-            <div key={movie.id}>{movie.title}</div>
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image}/>
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => <li>{g}</li>)}
+              </ul>
+            </div>
           )}
         </div>
       )}
